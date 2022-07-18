cfd75e89ee5a2c9010e6d92a6e0429038574395d
yangjingyi <yangjingyi0730@163.com>
2022-07-19 00:20:14 +0800

    update

---
+++ search/surrounding_area_130.py
@@ -0,0 +1,38 @@
+from typing import List
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != 'O':
+                return
+            board[i][j] = 'A'
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        if len(board) == 0 or len(board[0]) == 0:
+            return
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n - 1)
+        for i in range(n):
+            dfs(0, i)
+            dfs(m - 1, i)
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'A':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
+
+if __name__ == '__main__':
+    solution = Solution()
+    board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+    solution.solve(board)
+    print(board)
